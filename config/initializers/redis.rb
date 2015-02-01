7c43b8b64542dc66abc23fadfadf1f054f8a42e4
Mike Petersen <mike@odania-it.de>
2015-02-01 18:20:31 +0100

    Removed redis

--- config/initializers/redis.rb
+++
@@ -1,2 +0,0 @@
-redis_connection = Redis.new(:host => ENV['REDIS_1_PORT_6379_TCP_ADDR'], :port => ENV['REDIS_1_PORT_6379_TCP_PORT'])
-$redis = Redis::Namespace.new(:mission_control, :redis => redis_connection)
