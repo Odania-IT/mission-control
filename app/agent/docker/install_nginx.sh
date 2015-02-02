3ee708c4b42a6e12f9817a2b683e35d943f8896f
Mike Petersen <mike@odania-it.de>
2015-02-02 14:51:46 +0100

    Updated nginx config

--- app/agent/docker/install_nginx.sh
+++ app/agent/docker/install_nginx.sh
@@ -8,4 +8,5 @@ minimal_apt_get_install nginx
 ## Install haproxy runit service.
 mkdir /etc/service/nginx
 cp /srv/agent/docker/runit/nginx /etc/service/nginx/run
+cp /srv/agent/docker/config/nginx.conf /etc/nginx/nginx.conf
 touch /etc/service/nginx/down
