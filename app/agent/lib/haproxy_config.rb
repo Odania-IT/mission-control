5c411566aaea6a89bd7f688e087b6af40c07927c
Mike Petersen <mike@odania-it.de>
2015-02-02 01:51:16 +0100

    Updated haproxy template generator

--- app/agent/lib/haproxy_config.rb
+++ app/agent/lib/haproxy_config.rb
@@ -5,6 +5,28 @@ class HaproxyConfig
 		@applications = applications
 		@template = template
 		@server = $SERVER
+
+		get_all_ports
+	end
+
+	def get_all_ports
+		@all_ports = {}
+		@target_ports = {}
+		@image_on_port = {}
+		@applications.each do |application|
+			application.ports.each do |port|
+				splitted = port.split(':')
+
+				@all_ports[splitted[1]] = [] if @all_ports[splitted[1]].nil?
+				@all_ports[splitted[1]] << application
+
+				@target_ports[splitted[1]] = {} if @target_ports[splitted[1]].nil?
+				@target_ports[splitted[1]][application.id.to_s] = splitted[2]
+
+				@image_on_port[splitted[1]] = {} if @image_on_port[splitted[1]].nil?
+				@image_on_port[splitted[1]][application.id.to_s] = splitted[0]
+			end
+		end
 	end
 
 	def render
