bdfe786d733829caadc67171b4c890fab6f4d9b3
Mike Petersen <mike@odania-it.de>
2015-02-11 15:22:11 +0100

    Added test

--- spec/controllers/api/servers_controller_spec.rb
+++ spec/controllers/api/servers_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Api::ServersController, :type => :controller do
+	before (:each) do
+		@application = create(:application)
+	end
 
+	it 'lists all servers' do
+		get :index, {format: :json}
+		expect(response).to be_success
+		expect(response.header['Content-Type']).to include 'application/json'
+		expect(assigns(:servers)).to be_a(Mongoid::Criteria)
+		expect(response).to render_template('api/servers/index')
+	end
+
+	it 'sends a docker change if application is added' do
+	end
+
+	
 end
