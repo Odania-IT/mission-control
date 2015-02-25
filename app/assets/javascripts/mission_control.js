f2ff9d61ca93f4c309d654cd4ac220ee0811b115
Mike Petersen <mike@odania-it.de>
2015-02-25 18:52:11 +0100

    Revert "Updated links to reload page"
    This reverts commit 3e1b2ff36f6fd74792fe8c5505b58065bc4b681f.

--- app/assets/javascripts/mission_control.js
+++ app/assets/javascripts/mission_control.js
@@ -17,6 +17,159 @@ app.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpPr
 			});
 		}]
 	};
+
+	$routeProvider
+		.when('/dashboard', {
+			controller: 'DashboardController',
+			'templateUrl': config.getTemplatePath('dashboard/index'),
+			'resolve': resolve
+		})
+
+		.when('/servers', {
+			controller: 'ServersController',
+			'templateUrl': config.getTemplatePath('servers/index'),
+			'resolve': resolve
+		})
+		.when('/servers/new', {
+			controller: 'ServerEditController',
+			'templateUrl': config.getTemplatePath('servers/edit'),
+			'resolve': resolve
+		})
+		.when('/servers/:id/edit', {
+			controller: 'ServerEditController',
+			'templateUrl': config.getTemplatePath('servers/edit'),
+			'resolve': resolve
+		})
+		.when('/servers/:id', {
+			controller: 'ServerController',
+			'templateUrl': config.getTemplatePath('servers/show'),
+			'resolve': resolve
+		})
+
+		.when('/applications', {
+			controller: 'ApplicationsController',
+			'templateUrl': config.getTemplatePath('applications/index'),
+			'resolve': resolve
+		})
+		.when('/applications/new', {
+			controller: 'ApplicationEditController',
+			'templateUrl': config.getTemplatePath('applications/edit'),
+			'resolve': resolve
+		})
+		.when('/applications/:id/edit', {
+			controller: 'ApplicationEditController',
+			'templateUrl': config.getTemplatePath('applications/edit'),
+			'resolve': resolve
+		})
+		.when('/applications/:id', {
+			controller: 'ApplicationController',
+			'templateUrl': config.getTemplatePath('applications/show'),
+			'resolve': resolve
+		})
+
+		.when('/applications/:applicationId/images/new', {
+			controller: 'ImageEditController',
+			'templateUrl': config.getTemplatePath('images/edit'),
+			'resolve': resolve
+		})
+		.when('/applications/:applicationId/images/:id/edit', {
+			controller: 'ImageEditController',
+			'templateUrl': config.getTemplatePath('images/edit'),
+			'resolve': resolve
+		})
+		.when('/applications/:applicationId/images/:id', {
+			controller: 'ImageController',
+			'templateUrl': config.getTemplatePath('images/show'),
+			'resolve': resolve
+		})
+
+		.when('/repositories', {
+			controller: 'RepositoriesController',
+			'templateUrl': config.getTemplatePath('repositories/index'),
+			'resolve': resolve
+		})
+		.when('/repositories/new', {
+			controller: 'RepositoryEditController',
+			'templateUrl': config.getTemplatePath('repositories/edit'),
+			'resolve': resolve
+		})
+		.when('/repositories/:id/edit', {
+			controller: 'RepositoryEditController',
+			'templateUrl': config.getTemplatePath('repositories/edit'),
+			'resolve': resolve
+		})
+		.when('/repositories/:id', {
+			controller: 'RepositoryController',
+			'templateUrl': config.getTemplatePath('repositories/show'),
+			'resolve': resolve
+		})
+
+		.when('/backup', {
+			controller: 'BackupController',
+			'templateUrl': config.getTemplatePath('backup/index'),
+			'resolve': resolve
+		})
+
+		.when('/backup_servers/new', {
+			controller: 'BackupServerEditController',
+			'templateUrl': config.getTemplatePath('backup_servers/edit'),
+			'resolve': resolve
+		})
+		.when('/backup_servers/:id/edit', {
+			controller: 'BackupServerEditController',
+			'templateUrl': config.getTemplatePath('backup_servers/edit'),
+			'resolve': resolve
+		})
+		.when('/backup_servers/:id', {
+			controller: 'BackupServerController',
+			'templateUrl': config.getTemplatePath('backup_servers/show'),
+			'resolve': resolve
+		})
+
+		.when('/background_schedules/new', {
+			controller: 'BackgroundScheduleEditController',
+			'templateUrl': config.getTemplatePath('background_schedules/edit'),
+			'resolve': resolve
+		})
+		.when('/background_schedules/:id/edit', {
+			controller: 'BackgroundScheduleEditController',
+			'templateUrl': config.getTemplatePath('background_schedules/edit'),
+			'resolve': resolve
+		})
+		.when('/background_schedules/:id', {
+			controller: 'BackgroundScheduleController',
+			'templateUrl': config.getTemplatePath('background_schedules/show'),
+			'resolve': resolve
+		})
+
+		.when('/templates', {
+			controller: 'TemplatesController',
+			'templateUrl': config.getTemplatePath('templates/index'),
+			'resolve': resolve
+		})
+		.when('/templates/new', {
+			controller: 'TemplateEditController',
+			'templateUrl': config.getTemplatePath('templates/edit'),
+			'resolve': resolve
+		})
+		.when('/templates/:id/edit', {
+			controller: 'TemplateEditController',
+			'templateUrl': config.getTemplatePath('templates/edit'),
+			'resolve': resolve
+		})
+		.when('/templates/:id', {
+			controller: 'TemplateController',
+			'templateUrl': config.getTemplatePath('templates/show'),
+			'resolve': resolve
+		})
+
+		.when('/global_images', {
+			controller: 'GlobalImagesController',
+			'templateUrl': config.getTemplatePath('global_images/index'),
+			'resolve': resolve
+		})
+
+		.otherwise({ 'redirectTo': '/dashboard' });
 }]);
 
 app.run(['$rootScope', 'BootstrapResource', 'eventTypeProvider', '$sce', function ($rootScope, BootstrapResource, eventTypeProvider, $sce) {
